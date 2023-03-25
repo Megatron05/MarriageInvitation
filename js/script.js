f88b92bb1df08f2601714e721c732d8680c2be3f
Megatron <91841117+Megatron05@users.noreply.github.com>
2023-03-26 00:35:59 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+(function ($) {
+    "use strict";
+      $('.sakura-falling').sakura();
+})(jQuery);
+
+
+$(document).on('click', function(){
+    document.getElementById("my_audio").play();
+    console.log('Shaadi me zaroor aana');
+});
+
+
+
